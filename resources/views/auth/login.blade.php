689468b0a68ac31671519a29d39db8f438b04fbe
Muhammad Rizky <mrizky98@gmail.com>
2020-02-19 23:30:39 +0700

    first

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,6 +56,9 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
